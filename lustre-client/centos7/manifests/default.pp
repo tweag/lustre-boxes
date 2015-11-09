de370501e63de9ce41d16f540336a6c5b8dc0d6c
Mathieu Boespflug <m@tweag.io>
2015-11-09 23:08:34 +0100

    Generate lustre-client RPM's in /rpmbuild folder.

---
+++ lustre-client/centos7/manifests/default.pp
@@ -0,0 +1,41 @@
+vcsrepo { "/root/lustre.git":
+  ensure => present,
+  provider => git,
+  source => 'git://git.hpdd.intel.com/fs/lustre-release.git',
+  revision => '2.7.0',
+}
+
+package { 'python-docutils':
+  ensure => present,
+}
+
+package { 'libselinux-devel':
+  ensure => present,
+}
+
+exec { 'development-tools':
+  unless  => '/usr/bin/yum grouplist "Development tools" | /bin/grep "^Installed Groups"',
+  command => '/usr/bin/yum -y groupinstall "Development tools"',
+}
+
+exec { 'configure-lustre-spec':
+  command => 'sh autogen.sh &&
+              ./configure --with-linux=/usr/src/kernels/$(rpm -q --qf "%{VERSION}-%{RELEASE}.%{ARCH}" kernel-devel)/',
+  creates => "/root/lustre.git/lustre.spec",
+  cwd => "/root/lustre.git",
+  path => "/bin:/usr/bin",
+  require => [Vcsrepo["/root/lustre.git"],
+              Package['python-docutils'],
+              Exec['development-tools']],
+}
+
+exec { 'build-lustre-rpm':
+  command => 'make rpms',
+  cwd => "/root/lustre.git",
+  path => "/bin:/usr/bin",
+  require => [Vcsrepo["/root/lustre.git"],
+              Package['libselinux-devel'],
+              Exec['configure-lustre-spec']],
+  timeout => 0,
+}
+
